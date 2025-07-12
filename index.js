0d8714d229b2e94e8a3c5c88e19105aca09c6e58
SoumyaRanjanBhana1 <soumyabhanja180@gmail.com>
2025-07-12 18:33:32 +0530

    added

--- index.js
+++ index.js
@@ -59,6 +59,10 @@ app.post("/api/contact", async (req, res) => {
   }
 });
 
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the API!");
+});
 // Start Server
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
